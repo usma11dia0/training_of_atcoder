58123136a8a0a19012d009c53c277abbb9565917
usma11dia0 <usma11dia0@gmail.com>
2022-11-15 15:48:40 +0900

    add a06_how_many_guests

---
+++ kyopro-tessoku/chap02/submit_a06_how_many_guests.py
@@ -0,0 +1,30 @@
+# question https://atcoder.jp/contests/tessoku-book/tasks/math_and_algorithm_ai
+# answer
+
+# 数値の取得
+n, q = map(int, input().split())
+a = list(map(int, input().split()))
+
+question_list = []
+for i in range(0, q):
+    tmp = list(map(int, input().split()))
+    question_list.append(tmp)
+
+# 全体の累積和を計算
+accu = 0
+accu_list = []
+for a_num in a:
+    accu += a_num
+    accu_list.append(accu)
+
+for question in question_list:
+    l = question[0] - 1
+    r = question[1] - 1
+    # 1日目が開始の場合はn-1日目までの累計を引く
+    if l != 0:
+        ans = accu_list[r] - accu_list[l - 1]
+        print(ans)
+    # 1日目が開始の場合は引く必要なし
+    else:
+        ans = accu_list[r]
+        print(ans)
