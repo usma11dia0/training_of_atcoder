cbe4a709f51e27637d754ed8edbbba428494b39c
usma11dia0 <usma11dia0@gmail.com>
2022-11-16 18:01:41 +0900

    add a11_binary_search

---
+++ kyopro-tessoku/chap03/submit_a11_binary_search_1.py
@@ -0,0 +1,22 @@
+# question https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_k
+# answer
+
+# 教訓 式の()による計算順序に注意する。
+#      条件式の=有無について確認を忘れない。
+
+# 入力データ取得
+n, x = map(int, input().split())
+a = list(map(int, input().split()))
+
+# 二分探索の実施
+l = 0
+r = n
+while l <= r:
+    pivot_index = (l + r) // 2  # 小数点以下切り捨て
+    if x == a[pivot_index]:
+        print(pivot_index + 1)
+        break
+    elif x < a[pivot_index]:
+        r = pivot_index - 1
+    else:
+        l = pivot_index + 1
