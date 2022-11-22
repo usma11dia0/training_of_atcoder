222ec67581ecced93df5ab32989a206b13ab18bb
usma11dia0 <usma11dia0@gmail.com>
2022-11-22 19:09:14 +0900

    add_3 a21_block_game

--- kyopro-tessoku/chap04/submit_a21_submit_block_game.py
+++ kyopro-tessoku/chap04/submit_a21_submit_block_game.py
@@ -3,10 +3,24 @@
 # 参考　https://atcoder.jp/contests/tessoku-book/submissions/36580354
 
 # 教訓
-# # for文の順番に注意
+# for文の順番に注意
 # r-l(右端-左端)の大きい順に計算
 # r-lの値でfor文を逆から回す
 
+# 配列内に-1や+1を入れてFor文を回す際は、out_of_rangeを常に意識する。
+
+# for i in range(×, ×):
+# 	P[i], A[i] = map(int, input().split())の時の、
+# range(0,n)とrange(1,n+1)の違い
+# range(0, n) = [1,2,3,4,0]
+# range(1, n + 1) = [0,1,2,3,4]
+
+# reversedとrangeの違い
+# reversed(range(0,N-1)) = 0,1,2,3,4の逆
+# range(n-1, 0 -1) = 5,4,3,2,1 という違いがある点に注意
+
+
+# 考え方
 # 区間DP 二次元配列の第一要素と第二要素を区間に見立てて対応する。
 # dp[l][r]の条件 (最後にdp[l][r]になるための直前の条件を調べる)
 # dp[l-1][r]の状態から左端を取り除く
@@ -30,7 +44,7 @@
 n = int(input())
 p = [0] * (n + 1)  # index - 1 が添え字に対応
 a = [0] * (n + 1)  # index - 1 が添え字に対応
-for i in range(0, n):
+for i in range(1, n + 1):
     p[i], a[i] = map(int, input().split())
 
 
@@ -39,8 +53,11 @@ dp = [[0] * (n + 1) for _ in range(0, n + 1)]
 # for文の順番に注意
 # r-l(右端-左端)の大きい順に計算
 # → r-lの値でfor文を逆から回す
-# LEN = r-l区間
-for LEN in range(n - 1, 0, -1):  # 例: n=6の時, LEN = 6-1= 5とn-1個になる
+# LEN = r-l区間　例: n=6の時, LEN = 6-1= 5とn-1個になる
+for LEN in range(n - 2, -1, -1):
+    # reversed(range(0,N-1)) = 0,1,2,3,4の逆
+    # range(n-1, 0 -1) = 5,4,3,2,1 という違いがある点に注意
+
     # 左端: 1からどこまで?
     # n = 6,LEN=3の時、LENが3となるのは(1,4)(2,5)(3,6)の3つ。
     # 3という値はn - LEN + 1で導出
@@ -48,28 +65,31 @@ for LEN in range(n - 1, 0, -1):  # 例: n=6の時, LEN = 6-1= 5とn-1個にな�
         # 右端: 左端 + LEN
         r = l + LEN
 
-        # 得点の初期化
-        score_left = 0
-        score_right = 0
-
+        # 得点算出
         # 一番左のブロックを取り除く場合
-        # 取り除いた後、(l,r)の範囲内にp[l-1]のブロックが残っている時 = 加点
-        if l <= p[l - 1] <= r:
-            score_left = dp[l - 1][r] + a[l - 1]
-        # 取り除いた後、(l,r)の範囲内にp[l-1]のブロックが残っていない時
-        elif l > p[l - 1] and p[l - 1] > r:
-            score_left = dp[l - 1][r]
+        # 取り除いた後、(l,r)の範囲内にp[l-1]のブロックが残っている時に加点。それ以外は0点
+        score_left = 0
+        if l >= 2 and l <= p[l - 1] and p[l - 1] <= r:
+            score_left = a[l - 1]
 
         # 一番右のブロックを取り除く場合
-        # 取り除いた後、(l,r)の範囲内にp[r+1]のブロックが残っている時 = 加点
-        # LEN=1, l=3, r=4の時、a[r+1]はout_of_rangeとなるため、r<=n-1の制約を追加
-        elif r <= n - 1 and l <= p[r + 1] <= r:
-            score_right = dp[l][r + 1] + a[r + 1]
-        # 取り除いた後、(l,r)の範囲内にp[r+1]のブロックが残っていない時
-        elif l > p[l - 1] and p[l - 1] > r:
-            score_left = dp[l][r + 1]
+        # 取り除いた後、(l,r)の範囲内にp[r+1]のブロックが残っている時に加点。それ以外は0点
+        score_right = 0
+        if (
+            r <= n - 1 and l <= p[r + 1] and p[r + 1] <= r
+        ):  # r<=n-1の理由：例えばLEN=1, l=3の時、r=4となりp[r+1]はout_of_rangeとなるため、
+            score_right = a[r + 1]
 
-        # 得点導出
-        dp[l][r] = max(score_left, score_right)
+        # dp[l][r]の導出
+        if l == 1:  # l=1となる時は、全て一番右のブロックを選択した場合のみ。
+            dp[l][r] = dp[l][r + 1] + score_right
+        elif r == n:  # r=nとなる時は、全て一番左のブロックを選択した場合のみ。
+            dp[l][r] = dp[l - 1][r] + score_left
+        else:
+            dp[l][r] = max(dp[l - 1][r] + score_left, dp[l][r + 1] + score_right)
 
-print(dp)
+# 出力
+Answer = 0
+for i in range(1, n + 1):
+    Answer = max(Answer, dp[i][i])
+print(Answer)
