d8e6331dcefa42d8c39f50369bfec7fa37b75315
usma11dia0 <usma11dia0@gmail.com>
2022-12-01 17:22:14 +0900

    add a29_power

---
+++ kyopro-tessoku/chap05/submit_a29_power.py
@@ -0,0 +1,27 @@
+# question https://atcoder.jp/contests/tessoku-book/tasks/math_and_algorithm_aq
+# answer https://github.com/E869120/kyopro-tessoku/blob/main/codes/python/chap05/answer_A29.py
+
+# 教訓：
+
+# 入力データの取得
+
+# a の b 乗を m で割った余りを返す関数
+def Power(a, b, m):
+    p = a
+    Answer = 1
+    for i in range(30):
+        wari = 2**i
+        # bを二進数表記
+        if (b // wari) % 2 == 1:
+            # bを二進数表記して1になった場合、
+            Answer = (Answer * p) % m  # a の 2^i 乗が掛けられるとき
+            # 掛け算の場合は余りをいつ計算しても良いため、計算の都度余りを導出する。
+        p = (p * p) % m  # p=aの2^iを示している。
+    return Answer
+
+
+# 入力
+a, b = map(int, input().split())
+
+# 出力
+print(Power(a, b, 1000000007))
