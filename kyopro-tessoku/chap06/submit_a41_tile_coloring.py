1dc4eed605ae349a52d23d2accec1e6ad198a7f5
usma11dia0 <usma11dia0@gmail.com>
2022-12-08 23:07:17 +0900

    remove a41_tile_coloring

--- kyopro-tessoku/chap06/submit_a41_tile_coloring.py
+++
@@ -1,6 +0,0 @@
-# question https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_ao
-# answer https://github.com/E869120/kyopro-tessoku/blob/main/codes/python/chap06/answer_A41.py
-
-# 教訓：
-
-# 入力データ取得
