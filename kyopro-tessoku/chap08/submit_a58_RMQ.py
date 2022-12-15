fa0faa4ab18e6cc4ac6d4f77eed82e9447d43a78
usma11dia0 <usma11dia0@gmail.com>
2022-12-15 17:22:14 +0900

    add a58_RMQ

---
+++ kyopro-tessoku/chap08/submit_a58_RMQ.py
@@ -0,0 +1,10 @@
+# question https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_bf
+# answer https://github.com/E869120/kyopro-tessoku
+
+# 再起関数参考資料　C:\Users\usma1\OneDrive\ドキュメント\競プロノート\競技プログラミングの鉄則\a58
+
+# 教訓： セグメント木の再帰関数による作成方法
+# セグメント木：「○○番目から△△番目までの要素の最大値を求めてください」といった区間に関するクエリを得意とするデータ構造
+
+
+# 入力データの取得　(0-indexedで実装)
