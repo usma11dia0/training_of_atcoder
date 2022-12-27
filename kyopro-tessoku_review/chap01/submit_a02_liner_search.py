6c6eb96214d20f6c52670fc25f7a1624e8d8c7d1
usma11dia0 <usma11dia0@gmail.com>
2022-12-27 18:52:08 +0900

    add_review_a02_two_cards

--- kyopro-tessoku_review/chap01/submit_a02_liner_search.py
+++ kyopro-tessoku_review/chap01/submit_a02_liner_search.py
@@ -1,5 +1,14 @@
 # question https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_a
 # answer https://github.com/E869120/kyopro-tessoku/blob/main/codes/python/chap01/answer_A02.py
 
-# 数値の取得
+import sys
 
+# 数値の取得
+N, X = map(int, input().split())
+A = list(map(int, input().split()))
+
+for a in A:
+    if a == X:
+        print("Yes")
+        sys.exit(0)
+print("No")
