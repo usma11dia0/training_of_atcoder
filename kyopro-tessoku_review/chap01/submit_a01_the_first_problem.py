d0b78bbbaa2842004a497b9dee4a2f75919f9c8d
usma11dia0 <usma11dia0@gmail.com>
2022-12-27 18:46:53 +0900

    add_review_a01_the_first_problem

---
+++ kyopro-tessoku_review/chap01/submit_a01_the_first_problem.py
@@ -0,0 +1,7 @@
+# question https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_a
+# answer https://github.com/E869120/kyopro-tessoku/blob/main/codes/python/chap01/answer_A01.py
+
+# 数値の取得
+n = int(input())
+ans = n * n
+print(ans)
---
+++ kyopro-tessoku_review/chap01/submit_a02_liner_search.py
@@ -0,0 +1,5 @@
+# question https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_a
+# answer https://github.com/E869120/kyopro-tessoku/blob/main/codes/python/chap01/answer_A02.py
+
+# 数値の取得
+
