39d0e909520c295374e99c5b61d29923931edbab
usma11dia0 <usma11dia0@gmail.com>
2023-01-02 15:48:12 +0900

    add_review_a13_close_pairs_binary

--- kyopro-tessoku_review/chap03/submit_a13_close_pairs_binary.py
+++ kyopro-tessoku_review/chap03/submit_a13_close_pairs_binary.py
@@ -2,9 +2,10 @@
 # answer
 
 # 教訓：二分探索はK以上かどうかを判定
-# ※※下記のコードでなぜleft == rightの条件が満たされないか分からない
-# 二分探索の条件を <=K以下かどうかの判定としている。
-# Leftをmid-1して調整している。
+# Leftをmid-1して調整している。→ 二分探索ではleftは必ずmid + 1。mid-1ではleft=rightが満たされない。
+
+# 参考文献 python.bisectをスクラッチから実装
+# https://qiita.com/xu1718191411/items/42f6b32959be58458546
 
 # 入力データの取得
 N, K = map(int, input().split())
@@ -20,21 +21,21 @@ def binary_search(mid: int, A: list, K: int, index: int) -> bool:
         return False
 
 
-left = 0
-right = N - 1
-index = 0
-while left <= right:
-    print("test")
-    if left == right:
-        print(left)
-        break
-    mid = (left + right) // 2
-    if binary_search(mid, A, K, index):
-        # left = mid  ※収束しない
-        left = mid + 1
-    else:
-        # right = mid -1 ※収束しない
-        right = mid
+cnt = 0
+for i in range(0, N - 1):
+    left = 0
+    right = N - 1
+    while left <= right:
+        if left == right:
+            break
+        mid = (left + right) // 2
+        if binary_search(mid, A, K, i):
+            left = mid + 1
+        else:
+            right = mid
+    cnt += left - (i + 1)
 
+
+print(cnt)
 # left = 1, right = 2, mid = 1, index = 0
 # A[mid]=12, A[index]=11
