efb27aab2fff2096877a5438aca91bcbf764ca73
usma11dia0 <usma11dia0@gmail.com>
2023-01-08 14:23:29 +0900

    add_a13_review_close_pairs_syakutori_answer

--- kyopro-tessoku_review/chap03/submit_a13_close_pairs_syakutori.py
+++ kyopro-tessoku_review/chap03/submit_a13_close_pairs_syakutori.py
@@ -1,62 +1,33 @@
 # question https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_m
 # answer
 
-# 教訓：しゃくとり法の考え方の基礎は、
+# 教訓：しゃくとり法の考え方の基礎は、自明な範囲の探索を再度実行しないこと
 
 # 参考資料：https://www.akiradeveloper.com/post/syakutori/
 
 # 入力データ取得
-n, k = map(int, input().split())
-a = list(map(int, input().split()))
+N, K = map(int, input().split())
+A = list(map(int, input().split()))  # 0-indexed
 
-# 配列の準備　#要素番号iのもとで、問題文の条件を満たす最大のAの要素番号の配列
-r = [None] * n
+# 尺取り法の実装
 
-# しゃくとり法の実施
-# n番目は該当なしのため。
-for i in range(0, n - 1):
+# 配列の初期化(解の格納先)
+# r[i]: A[r[i]+1] - A[i] <= K を満たす最大の要素番号+1
+r = [None] * N
 
-    # スタート位置の指定
+for i in range(0, N - 1):
+    # 初期値の設定
     if i == 0:
         r[i] = 0
-    else:  # iが0以外の場合
-        r[i] = r[i - 1]  # 更新されたiのr[i]の値を求める際は、i-1で確定したrの値からスタートする。
+    else:
+        r[i] = r[i - 1]
 
-    # r[i]の値が配列Aの要素番号を超えない,かつ要素番号r[i]のもとでさえ条件を満たす場合
-    while r[i] < n - 1 and a[r[i] + 1] - a[i] <= k:
-        # 要素番号r[i]の値を1増やす(上記で条件を満たしており、r[i]が最大ではないため)
+    while r[i] != N - 1 and A[r[i] + 1] - A[i] <= K:
         r[i] += 1
-    # whileループを抜けた場合、要素番号r[i]はiの下では条件を満たす最大の要素番号となっている。
 
 # 結果の出力
 ans = 0
-for i in range(0, n - 1):
+for i in range(0, N - 1):
     ans += r[i] - i
 
 print(ans)
-
-
-# # 入力
-# n, k = map(int, input().split())
-# a = list(map(int, input().split()))
-
-# # 配列の準備
-# r = [None] * n
-
-# # しゃくとり法
-# for i in range(0, n - 1):
-#     # スタート地点を決める
-#     if i == 0:
-#         r[i] = 0
-#     else:
-#         r[i] = r[i - 1]
-
-#     # ギリギリまで増やしていく
-#     while r[i] < n - 1 and a[r[i] + 1] - a[i] <= k:
-#         r[i] += 1
-
-# # 出力
-# Answer = 0
-# for i in range(0, n - 1):
-#     Answer += r[i] - i
-# print(Answer)
