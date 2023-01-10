50b4fe0ec30d37b6662dc9e32577758337ec0923
usma11dia0 <usma11dia0@gmail.com>
2023-01-10 23:24:39 +0900

    add_review_a17_dungeon2

---
+++ kyopro-tessoku_review/chap04/submit_a17_dungeon2.py
@@ -0,0 +1,4 @@
+# question https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_q
+# answer https://github.com/E869120/kyopro-tessoku/blob/main/codes/python/chap04/answer_A17.py
+
+# 教訓　部屋番号とインデックスを一致させるためには要素数+1にする
