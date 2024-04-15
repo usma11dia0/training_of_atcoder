1c48db9e6eb479e7f76a3173ce5ee232e4fc52ac
usma11dia0 <usma11dia0@gmail.com>
2024-04-15 10:49:08 +0900

    add: cpp env

---
+++ apg4b/1.0.0/test.cpp
@@ -0,0 +1,7 @@
+// https://seiseiengineering.com/atcoder%e3%81%aec%e7%92%b0%e5%a2%83%e6%a7%8b%e7%af%89-vscodewsl2%e3%81%a7%e5%bf%ab%e9%81%a9%e3%81%ab%ef%bc%81/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  cout << "Hello, world!" << endl;
+}
